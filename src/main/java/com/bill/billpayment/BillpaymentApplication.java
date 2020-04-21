a5c1a21e58f54e3f9405997ee67905787e96233f
sucharitha-vallabhaneni <suchi.pretty.5@gmail.com>
2020-04-21 11:21:27 +0530

    intial commit

---
+++ src/main/java/com/bill/billpayment/BillpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.bill.billpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillpaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bill/billpayment/BillpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.billpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
